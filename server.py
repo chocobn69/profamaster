da0684ee7aafe16bd2faf4a8d858eb4677c7c2ca
Nicolas Baccelli <nicolas.baccelli@abc-culture.fr>
2018-07-02 13:49:39 +0200

    Add functionnal skeleton

--- server.py
+++ server.py
@@ -3,6 +3,19 @@ from aiohttp import web
 
 logger = logging.getLogger(__name__)
 
+TIME_BETEWEEN_EXEC = 500
+
+
+def pane_movement(p_number, way, percent_move=1.0):
+    """ move up/down pane number p_number to x percent """
+    if way is not in ['up', 'down']:
+        raise AttributeError('way has to be either up or down')
+
+
+async def exec_orders_queue():
+    """ read fifo queue and execute orders sequentialy """
+    pass
+
 async def handle(request):
     name = request.match_info.get('name', "Anonymous")
     text = "Hello, " + name
