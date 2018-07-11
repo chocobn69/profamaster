55beb399cf06c3363ee66cf0d153a0d7bce13311
Nicolas Baccelli <nicolas.baccelli@abc-culture.fr>
2018-07-11 13:36:44 +0200

    Makes tests ok

--- profamaster/tests/test_server.py
+++ profamaster/tests/test_server.py
@@ -19,61 +19,68 @@ class ProfamasterTestCase(AioHTTPTestCase):
         resp = await self.client.request(method, url)
         self.assertEqual(resp.status, asserted_status)
         text = await resp.text()
-        self.assertDictEqual(asserted_response,
-                             json.loads(text))
+        if asserted_response:
+            self.assertDictEqual(asserted_response,
+                                 json.loads(text))
 
     @unittest_run_loop
     async def test_root(self):
-        self._test_call('GET', '/', 200, {'status': 'ok'})
+        await self._test_call('GET', '/', 200, {'status': 'ok'})
+
+        # try 404
+        await self._test_call('GET', '/toto', 404, None)
 
     @unittest_run_loop
     async def test_actions(self):
 
         # first, try legit actions
         # test action up on pane 1
-        self._test_call('POST', '/action/1/up', 200, {'status': 'ok'})
+        await self._test_call('POST', '/action/1/up', 200, {'status': 'ok'})
 
         # test action stop on pane 1
-        self._test_call('POST', '/action/1/stop', 200, {'status': 'ok'})
+        await self._test_call('POST', '/action/1/stop', 200, {'status': 'ok'})
 
         # test action down on pane 1
-        self._test_call('POST', '/action/1/down', 200, {'status': 'ok'})
+        await self._test_call('POST', '/action/1/down', 200, {'status': 'ok'})
+
+        # try with not POST method
+        await self._test_call('GET', '/action/1/up', 404, None)
 
         # now try with unknown pane
         # test action up on pane 9
-        self._test_call('POST', '/action/9/up', 503,
-                        {
-                            'status': 'error',
-                            'error_code': 101,
-                            'message': 'unknown pane',
-                        })
+        await self._test_call('POST', '/action/9/up', 503,
+                              {
+                                  'status': 'error',
+                                  'error_code': 101,
+                                  'message': 'unknown pane',
+                              })
 
         # test action stop on pane 9
-        self._test_call('POST', '/action/9/stop', 503,
-                        {
-                            'status': 'error',
-                            'error_code': 101,
-                            'message': 'unknown pane',
-                        })
+        await self._test_call('POST', '/action/9/stop', 503,
+                              {
+                                  'status': 'error',
+                                  'error_code': 101,
+                                  'message': 'unknown pane',
+                              })
 
         # test action down on pane 9
-        self._test_call('POST', '/action/9/down', 503,
-                        {
-                            'status': 'error',
-                            'error_code': 101,
-                            'message': 'unknown pane',
-                        })
+        await self._test_call('POST', '/action/9/down', 503,
+                              {
+                                  'status': 'error',
+                                  'error_code': 101,
+                                  'message': 'unknown pane',
+                              })
 
         # now try with non legit actions
-        self._test_call('POST', '/action/1/stale', 503,
-                        {
-                            'status': 'error',
-                            'error_code': 102,
-                            'message': 'unknown action',
-                        })
-        self._test_call('POST', '/action/1/948759348759', 503,
-                        {
-                            'status': 'error',
-                            'error_code': 102,
-                            'message': 'unknown action',
-                        })
+        await self._test_call('POST', '/action/1/stale', 503,
+                              {
+                                  'status': 'error',
+                                  'error_code': 102,
+                                  'message': 'unknown action',
+                              })
+        await self._test_call('POST', '/action/1/948759348759', 503,
+                              {
+                                  'status': 'error',
+                                  'error_code': 102,
+                                  'message': 'unknown action',
+                              })
