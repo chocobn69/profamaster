14c9a3bbe665bf971c199b3f7460cf8027dc570f
Nicolas Baccelli <nicolas.baccelli@abc-culture.fr>
2018-07-11 13:32:11 +0200

    TDD create API tests before coding it

--- profamaster/tests/test_server.py
+++ profamaster/tests/test_server.py
@@ -1,5 +1,5 @@
 from aiohttp.test_utils import AioHTTPTestCase, unittest_run_loop
-from aiohttp import web
+import json
 
 from ..web import start_server
 
@@ -7,27 +7,73 @@ from ..web import start_server
 class ProfamasterTestCase(AioHTTPTestCase):
 
     async def get_application(self):
-
+        """ get app from web.start_server """
         app = await start_server()
         return app
 
-    # the unittest_run_loop decorator can be used in tandem with
-    # the AioHTTPTestCase to simplify running
-    # tests that are asynchronous
-    @unittest_run_loop
-    async def test_example(self):
-        resp = await self.client.request("GET", "/")
-        assert resp.status == 200
+    async def _test_call(self,
+                         method='GET',
+                         url='/',
+                         asserted_status=200,
+                         asserted_response={}):
+        resp = await self.client.request(method, url)
+        self.assertEqual(resp.status, asserted_status)
         text = await resp.text()
-        assert "Hello, Anonymous" in text
+        self.assertDictEqual(asserted_response,
+                             json.loads(text))
 
-    # a vanilla example
-    def test_example_vanilla(self):
-        async def test_get_route():
-            url = "/"
-            resp = await self.client.request("GET", url)
-            assert resp.status == 200
-            text = await resp.text()
-            assert "Hello, Anonymous" in text
+    @unittest_run_loop
+    async def test_root(self):
+        self._test_call('GET', '/', 200, {'status': 'ok'})
 
-        self.loop.run_until_complete(test_get_route())
+    @unittest_run_loop
+    async def test_actions(self):
+
+        # first, try legit actions
+        # test action up on pane 1
+        self._test_call('POST', '/action/1/up', 200, {'status': 'ok'})
+
+        # test action stop on pane 1
+        self._test_call('POST', '/action/1/stop', 200, {'status': 'ok'})
+
+        # test action down on pane 1
+        self._test_call('POST', '/action/1/down', 200, {'status': 'ok'})
+
+        # now try with unknown pane
+        # test action up on pane 9
+        self._test_call('POST', '/action/9/up', 503,
+                        {
+                            'status': 'error',
+                            'error_code': 101,
+                            'message': 'unknown pane',
+                        })
+
+        # test action stop on pane 9
+        self._test_call('POST', '/action/9/stop', 503,
+                        {
+                            'status': 'error',
+                            'error_code': 101,
+                            'message': 'unknown pane',
+                        })
+
+        # test action down on pane 9
+        self._test_call('POST', '/action/9/down', 503,
+                        {
+                            'status': 'error',
+                            'error_code': 101,
+                            'message': 'unknown pane',
+                        })
+
+        # now try with non legit actions
+        self._test_call('POST', '/action/1/stale', 503,
+                        {
+                            'status': 'error',
+                            'error_code': 102,
+                            'message': 'unknown action',
+                        })
+        self._test_call('POST', '/action/1/948759348759', 503,
+                        {
+                            'status': 'error',
+                            'error_code': 102,
+                            'message': 'unknown action',
+                        })
